bc5d52298e2da44339f0fb91cf6895a9f3c369bd
troglodisme <giulio.ammendola@network.rca.ac.uk>
2024-09-23 13:02:11 +0200

    Initial Commit

---
+++ RobotArmController/RobotArmControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotArmControllerApp.swift
+//  RobotArmController
+//
+//  Created by Giulio on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RobotArmControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
